86b61cdaf5f53ad5bc8e36ebc2d21a4cac6900a6
Nadermogib <nadermogib@gmail.com>
2025-12-02 18:35:25 +0300

    first_name fixed

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -11,12 +11,12 @@ const syncUser=inngest.createFunction(
     {event:"clerk/user.created"},
     async({event})=>{
         await connectDB();
-        const {id,email_addresses,frist_name,last_name,image_url}=event.data
+        const {id,email_addresses,first_name,last_name,image_url}=event.data
 
         const newUser ={
             clerkId:id,
             email:email_addresses[0]?.email_address,
-            name:`${frist_name || ""} ${last_name || ""}` || "User",
+            name:`${first_name || ""} ${last_name || ""}` || "User",
             imageUrl:image_url,
             addresses:[],
             wishList:[],
