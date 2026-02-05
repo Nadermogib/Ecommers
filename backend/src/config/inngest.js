c2382e9f6da82029f1843a0fb3c2c197479da5b6
Nadermogib <nadermogib@gmail.com>
2026-02-05 22:03:29 +0300

    paymentConfigration

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,40 +1,41 @@
 import { Inngest } from "inngest";
-import {connectDB } from "./db.js"
+import { connectDB } from "./db.js";
 
 import { User } from "../models/user.model.js";
 
-export const inngest=new Inngest({id:"ecommerc-inngest-app"})
- 
-// create user using inngest 
-const syncUser=inngest.createFunction(
-    {id:"sync-user"},
-    {event:"clerk/user.created"},
-    async({event})=>{
-        await connectDB();
-        const {id,email_addresses,first_name,last_name,image_url}=event.data
+export const inngest = new Inngest({ id: "ecommerc-inngest-app" });
 
-        const newUser ={
-            clerkId:id,
-            email:email_addresses[0]?.email_address,
-            name:`${first_name || ""} ${last_name || ""}` || "User",
-            imageUrl:image_url,
-            addresses:[],
-            wishList:[],
-        }
-        await User.create(newUser)
-    }
+// create user using inngest
+const syncUser = inngest.createFunction(
+  { id: "sync-user" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
+    const { id, email_addresses, first_name, last_name, image_url } =
+      event.data;
+
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}` || "User",
+      imageUrl: image_url,
+      addresses: [],
+      wishList: [],
+    };
+    await User.create(newUser);
+  },
 );
 
 // delete user from db using inngest
-const deleteUserFromDB=inngest.createFunction(
-    {id:"delete-user-from-db"},
-    {event:"clerk/user.deleted"},
-    async({event})=>{
-        await connectDB()
+const deleteUserFromDB = inngest.createFunction(
+  { id: "delete-user-from-db" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
 
-        const {id}=event.data;
-        await User.deleteOne({clerkId:id})
-    }
-)
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
+  },
+);
 
-export  const functions =[syncUser,deleteUserFromDB]
+export const functions = [syncUser, deleteUserFromDB];
---
+++ backend/src/controllers/payment.controller.js
@@ -0,0 +1,100 @@
+import Stripe from "stripe"
+import { ENV } from "../config/env.js"
+import { User } from "../models/user.model.js"
+import { Product } from "../models/product.model.js"
+import { Order } from "../models/order.model.js"
+import { Cart } from "../models/cart.model.js"
+
+const stripe=new Stripe(ENV.STRIPE_SECRET_KEY)
+export async function createPaymentIntent(req,res){
+    try {
+
+        const{cartItems,shippingAddress}=req.body
+        const user=req.user;
+
+        //validate cartItems
+        if(!cartItems || cartItems.length ===0){
+            return res.status(400).json({error:"Cart is empty"})
+        }
+
+        //calculet total amount from server-side (dont trust client-ever)
+        let subtotal=0
+        const validatedItems=[]
+
+        for(const item of cartItems){
+            const product =await Product.findById(item.product._id)
+            if(!product){
+              return  res.status(404).json({error:`product${item.product.name} not found`})
+
+            }
+            if(product.stock <item.quantity){
+              return  res.status(400).json({error:`Insufficient stock of${item.product.name}`})   
+            }
+
+            subtotal+=product.price * product.quantity
+
+            validatedItems.push({
+                product:product._id.toString(),
+                name:product.name,
+                quantity:item.quantity,
+                image:product.images[0],
+            })
+        }
+
+        const shipping=10.0;//10$
+        const tax=subtotal *0.08 //8%
+        const total=subtotal+shipping+tax
+
+        if(total<=0){
+         return  res.status(404).json({error:"Invalid order total"})
+
+        }
+
+        //find or create the stripe customer
+        let customer;
+        if(user.stripeCustomerId){
+            //find the customer
+            customer=await stripe.customers.retrieve(user.stripeCustomerId)
+        }else{
+            //create the customer
+            customer=await stripe.customers.create({
+                email:user.email,
+                name:user.name,
+                metadata:{
+                   clerkId:user.clerkId,
+                   userId:user._id.toString()
+
+                }
+
+            })
+            await User.findByIdAndUpdate(user._id,{stripeCustomerId:customer.id})
+        }
+
+        //create payment intent
+
+        const paymentIntent=await stripe.paymentIntents.create({
+            amount:Math.round(total *100),//convert to Cents
+            currency:"usd",
+            customer:customer.id,
+            automatic_payment_methods:{
+                enabled:true,
+            },
+            mandate:{
+                clerkId:user.clerkId,
+                userId:user._id.toString(),
+                orderItems:JSON.stringify(validatedItems),
+                shippingAddress:JSON.stringify(shippingAddress),
+                totalPrice:total.toFixed(2)
+            }
+
+        })
+
+        res.status(200).json({clientSecret:paymentIntent.client_secret})
+
+        
+       } catch (error) {
+        console.log("Erorr creating payment intent",error)
+
+          res.status(500).json({error:"failed to creaet payment intent"})   
+        }
+    }
