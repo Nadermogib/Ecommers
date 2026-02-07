1d949a0880db7d94b14615b562096d33f54dfbf5
Nadermogib <nadermogib@gmail.com>
2026-02-08 01:07:19 +0300

    paymentFixed5

--- backend/src/models/order.model.js
+++ backend/src/models/order.model.js
@@ -69,7 +69,7 @@ const orderSchema = new mongoose.Schema(
     clerkId: {
       type: String,
       required: true,
-      unique: true,
+     
     },
     orderItems: [orderItemSchema],
     shippingAddress: {
